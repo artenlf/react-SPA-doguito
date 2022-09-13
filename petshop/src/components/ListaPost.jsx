b0a2515593edb31211b5f3cd20dd1eae3520408f
artenlf <felipe.arten@gmail.com>
2022-09-13 15:47:00 -0300

    Códigos Aula 8

--- petshop/src/components/ListaPost.jsx
+++ petshop/src/components/ListaPost.jsx
@@ -12,7 +12,7 @@ const ListaPost = ({ url }) => {
   return (
     <section className="posts container">
       {posts.map((post) => (
-        <Link className={`cartao-post cartao-post--${post.categoria}`}>
+        <Link className={`cartao-post cartao-post--${post.categoria}`} to={`/posts/${post.id}`}>
           <article key={post.id}>
             <h3 className="cartao-post__titulo">{post.title}</h3>
             <p className="cartao-post__meta">{post.metadescription}</p>
