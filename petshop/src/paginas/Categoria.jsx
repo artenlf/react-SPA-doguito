e066321caa2ca1b43ab98069febd9052bbb9e38f
artenlf <felipe.arten@gmail.com>
2022-09-13 23:44:21 -0300

    Códigos Aula 12

--- petshop/src/paginas/Categoria.jsx
+++ petshop/src/paginas/Categoria.jsx
@@ -1,12 +1,21 @@
-import React from "react";
-import { Route, useParams, useRouteMatch } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, Route, Switch, useParams, useRouteMatch } from "react-router-dom";
 import "../assets/css/blog.css";
+import { busca } from "../api/api";
 import ListaCategorias from "../components/ListaCategorias";
 import ListaPost from "../components/ListaPost";
+import SubCategoria from "./SubCategoria";
 
 const Categoria = () => {
   const { id } = useParams();
-  const { path } = useRouteMatch();
+  const { url, path } = useRouteMatch();
+  const [subcategorias, setSubCategorias] = useState([]);
+
+  useEffect(() => {
+    busca(`/categorias/${id}`, (categoria) => {
+      setSubCategorias(categoria.subcategorias);
+    });
+  }, [id]);
 
   return (
     <>
@@ -14,9 +23,23 @@ const Categoria = () => {
         <h2 className="titulo-pagina">Pet Notícias</h2>
       </div>
       <ListaCategorias />
-      <Route exact path={`${path}/`}>
-        <ListaPost url={`/posts?categoria=${id}`} />
-      </Route>
+      <ul className="lista-categorias container flex">
+        {subcategorias.map((subcategoria) => {
+          return (
+            <li className={`lista-categorias__categoria lista-categorias__categoria--${id}`} key={subcategoria}>
+              <Link to={`${url}/${subcategoria}`}>{subcategoria}</Link>
+            </li>
+          );
+        })}
+      </ul>
+      <Switch>
+        <Route exact path={`${path}/`}>
+          <ListaPost url={`/posts?categoria=${id}`} />
+        </Route>
+        <Route path={`${path}/:subcategoria`}>
+          <SubCategoria />
+        </Route>
+      </Switch>
     </>
   );
 };
